55e44132a1c1d534a7258b9897ea8aa3433929ce
Alvin Dennis <alvindennis80@gmail.com>
2026-01-11 20:59:11 +0530

    feat: add not found page to the route

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import NotFoundPage from "@/components/NotFound";
+
+function NotFound() {
+    return (
+        <div>
+            <NotFoundPage />
+        </div>
+    );
+}
+
+export default NotFound;
